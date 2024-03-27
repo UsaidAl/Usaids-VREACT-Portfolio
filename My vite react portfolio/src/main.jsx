9a92495c94ad9bb8ce473ab2fcd623d94510f609
UsaidAl <ali_usaid98@hotmail.com>
2024-03-27 23:47:07 +0000

    main.jsx done

--- My vite react portfolio/src/main.jsx
+++ My vite react portfolio/src/main.jsx
@@ -1,10 +1,53 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App.js"
+import reportWebVitals from "./reportWebVitals";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home.js";
+import About from "./pages/About.js";
+import Portfolio from "./pages/Portfolio.js";
+import Resume from "./pages/Resume.js";
+import Contact from "./pages/Contact.js";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+
+const routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+  {
+    path: "/portfolio",
+    element: <Portfolio />,
+  },
+  {
+    path: "/resume",
+    element: <Resume/>,
+  },
+  {
+    path: "/contact",
+    element: <Contact/>,
+  },
+  
+
+  
+
+]);
+
+const root =
+ ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
     <App />
+    <RouterProvider router={routes} />
   </React.StrictMode>,
-)
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
