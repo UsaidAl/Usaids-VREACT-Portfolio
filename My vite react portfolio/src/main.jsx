c298d5582e6c836cb452425005e029b4dbb1c369
UsaidAl <ali_usaid98@hotmail.com>
2024-03-27 23:48:56 +0000

    app.jsx route

--- My vite react portfolio/src/main.jsx
+++ My vite react portfolio/src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App.js"
+import App from "./App.jsx"
 import reportWebVitals from "./reportWebVitals";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./pages/Home.js";
